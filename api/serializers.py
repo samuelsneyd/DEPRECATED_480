13e2b475459ac912f08c40728748c64382a7e72b
Samuel <samuelsneyd@gmail.com>
2022-04-02 18:00:26 +1300

    Add api serializer class

---
+++ api/serializers.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+
+
+# class SerializerTest(serializers.ModelSerializer):
+#     class Meta:
+#         model = "model_goes_here"
+#         fields = ("field1", "field2")

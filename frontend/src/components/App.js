443ff96a90635d926b8a2380a72f82288c9f5eb6
Samuel <samuelsneyd@gmail.com>
2022-05-14 15:08:29 +1200

    Init React frontend

---
+++ frontend/src/components/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+export default function App() {
+  return <h1>Hello, world</h1>
+}
+
+const appDiv = document.getElementById('app');
+render(<App/>, appDiv);

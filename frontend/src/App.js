e922d74681c1068d2745fea9edf07d2706fb54a9
Samuel <samuelsneyd@gmail.com>
2022-05-15 13:30:34 +1200

    Add terms and privacy placeholders
    Add terms and privacy markdown placeholder files.
    Add markdown component to parse terms/privacy md files.
    Add Terms and Privacy page components.
    Add React and Django routing for Terms and Privacy pages.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,29 @@
-import React from 'react';
+import * as React from 'react';
 import { render } from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { Route, Routes } from 'react-router';
 import HomePage from './pages/HomePage/HomePage';
+import Terms from './pages/Terms/Terms';
+import Privacy from './pages/Privacy/Privacy';
 
 function App() {
-  return <HomePage/>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<HomePage/>}/>
+        <Route path="terms" element={<Terms/>}/>
+        <Route path="privacy" element={<Privacy/>}/>
+        <Route
+          path="*"
+          element={
+            <main style={{ padding: '1rem' }}>
+              <p>There's nothing here!</p>
+            </main>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 const appDiv = document.getElementById('app');
--- frontend/views.py
+++ frontend/views.py
@@ -1,5 +1,5 @@
 from django.views.generic.base import TemplateView
 
 
-class HomePageView(TemplateView):
+class IndexView(TemplateView):
     template_name = "frontend/index.html"

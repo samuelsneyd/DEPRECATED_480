4ed78e5359cb4d6ff49aac02a4eb6ddc34823770
Samuel <samuelsneyd@gmail.com>
2022-05-29 21:05:20 +1200

    Add frontend urls django unit tests

---
+++ frontend/tests/test_urls.py
@@ -0,0 +1,51 @@
+from django.test import TestCase
+from django.urls import reverse, resolve
+from frontend.views import IndexView
+
+
+class TestApiUrls(TestCase):
+    def setUp(self) -> None:
+        return
+
+    def test_index_url_is_resolved(self) -> None:
+        resolver = resolve(reverse("frontend:index"))
+        self.assertEqual(resolver.func.view_class, IndexView)
+
+    def test_accommodation_url_is_resolved(self) -> None:
+        resolver = resolve(reverse("frontend:accommodation"))
+        self.assertEqual(resolver.func.view_class, IndexView)
+
+    def test_location_url_is_resolved(self) -> None:
+        resolver = resolve(reverse("frontend:location"))
+        self.assertEqual(resolver.func.view_class, IndexView)
+
+    def test_activities_url_is_resolved(self) -> None:
+        resolver = resolve(reverse("frontend:activities"))
+        self.assertEqual(resolver.func.view_class, IndexView)
+
+    def test_book_url_is_resolved(self) -> None:
+        resolver = resolve(reverse("frontend:book"))
+        self.assertEqual(resolver.func.view_class, IndexView)
+
+    def test_contact_url_is_resolved(self) -> None:
+        resolver = resolve(reverse("frontend:contact"))
+        self.assertEqual(resolver.func.view_class, IndexView)
+
+    def test_about_url_is_resolved(self) -> None:
+        resolver = resolve(reverse("frontend:about"))
+        self.assertEqual(resolver.func.view_class, IndexView)
+
+    def test_terms_url_is_resolved(self) -> None:
+        resolver = resolve(reverse("frontend:terms"))
+        self.assertEqual(resolver.func.view_class, IndexView)
+
+    def test_privacy_url_is_resolved(self) -> None:
+        resolver = resolve(reverse("frontend:privacy"))
+        self.assertEqual(resolver.func.view_class, IndexView)
+
+    def test_faq_url_is_resolved(self) -> None:
+        resolver = resolve(reverse("frontend:faq"))
+        self.assertEqual(resolver.func.view_class, IndexView)
+
+    def tearDown(self) -> None:
+        return
--- frontend/urls.py
+++ frontend/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import IndexView
 
+app_name = "frontend"
+
 urlpatterns = [
     path("", IndexView.as_view(), name="index"),
     path("accommodation/", IndexView.as_view(), name="accommodation"),

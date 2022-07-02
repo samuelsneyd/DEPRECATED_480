b0524eb9308f4df7ce34183700e256b186c7697d
Samuel <samuelsneyd@gmail.com>
2022-07-02 14:03:20 +1200

    Init google maps location component

--- frontend/src/pages/LocationPage/LocationPage.js
+++ frontend/src/pages/LocationPage/LocationPage.js
@@ -1,11 +1,13 @@
 import * as React from 'react';
 import { Container } from '@mui/material';
+import GoogleMapsLocation from '../../components/GoogleMapsLocation/GoogleMapsLocation';
 import PageTitle from '../../components/PageTitle/PageTitle';
 
 const LocationPage = () => {
   return (
     <Container>
       <PageTitle title={'Location'} />
+      <GoogleMapsLocation />
     </Container>
   );
 };

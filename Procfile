c0b35cd20086b3804f4ef65f00be6802d882e520
Samuel <samuelsneyd@gmail.com>
2022-07-11 10:05:45 +1200

    Add python migration stage to procfile
    Mock .md file for React unit tests.

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
+release: python manage.py migrate
 web: gunicorn retreat_480.wsgi
--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -4,6 +4,7 @@ import App from './App';
 
 jest.mock('../static/legal/privacy.md', () => () => '<div>Privacy</div>');
 jest.mock('../static/legal/terms.md', () => () => '<div>Terms</div>');
+jest.mock('../static/faq/faq.md', () => () => '<div>FAQ</div>');
 
 it('renders App without crashing', () => {
   const div = document.createElement('div');
